96d100ef9cd57ec0203dce560003653aee70e86d
Binay Budhathoki <binay_budhathoki@hotmail.com>
2013-12-21 12:48:48 +0545

    add protweet model

--- protweet/protweet/apps/core/models.py
+++ protweet/protweet/apps/core/models.py
@@ -1,3 +1,35 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+#UserProfile Model
+class UserProfile(models.Model):
+	user = models.ForeignKey(User, related_name = 'user', db_index = True)
+	follower_count = models.IntegerField(default = 0)
+	following_count = models.IntegerField(default = 0)
+	tweet_count = models.IntegerField(default = 0)
+	profile_pic = models.ImageField(upload_to = 'profile_pic')
+	user_creation_timestamp = models.DateTimeField(auto_now = False, auto_now_add = True, editable = False)
+	
+	def __unicode__(self):
+		return u'%s' % (self.user)
+
+#Tweet Model
+class Tweet(models.Model):
+	tweet_userprofile = models.ForeignKey(UserProfile, related_name = 'tweet_userprofile', db_index = True)
+	parent_tweet_id = models.IntegerField(null = True, blank = True)
+	tweet = models.TextField(blank = False, null = False)
+	post_timestamp = models.DateTimeField(auto_now = False, auto_now_add = True, editable = False)
+	
+	def __unicide__(self):
+		return u'%s' % (self.tweet_userprofile)
+
+#Following_Follower Model
+class FollowingFollower(models.Model):
+	tweet_follower = models.ForeignKey(UserProfile, related_name = 'tweet_follower', db_index = True)
+	tweet_following = models.ForeignKey(UserProfile, related_name = 'tweet_following', db_index = True)
+	
+	def __unicode__(self):
+		return u'%s - %s' % (self.follower, self.following)
+	
+	
